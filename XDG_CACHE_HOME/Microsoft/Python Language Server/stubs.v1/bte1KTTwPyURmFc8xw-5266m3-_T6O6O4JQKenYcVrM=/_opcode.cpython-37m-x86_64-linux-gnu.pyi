249c99872ab1c375dd84c4ca8c57257d4605c355
marciorasf <marciorasf@gmail.com>
2019-12-03 09:44:13 -0300

    removing useless files and folders

--- XDG_CACHE_HOME/Microsoft/Python Language Server/stubs.v1/bte1KTTwPyURmFc8xw-5266m3-_T6O6O4JQKenYcVrM=/_opcode.cpython-37m-x86_64-linux-gnu.pyi
+++
@@ -1,8 +0,0 @@
-__doc__ = 'Opcode support module.'
-__file__ = '/home/marciorasf/github/ComputacaoEvolucionaria-2019.1/ce_venv/lib/python3.7/lib-dynload/_opcode.cpython-37m-x86_64-linux-gnu.so'
-__name__ = '_opcode'
-__package__ = ''
-def stack_effect(opcode, oparg):
-    'Compute the stack effect of the opcode.'
-    pass
-
